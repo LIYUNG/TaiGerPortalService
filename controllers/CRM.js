aec637377ca0dc63e54e881fee0085eccc1e159f
Alexander Jensen <ajexsen@users.noreply.github.com>
2025-06-29 23:26:28 +0200

    refactor: rename transcripts endpoint to meeting summaries and update controller logic

--- controllers/CRM.js
+++ controllers/CRM.js
@@ -1,13 +1,16 @@
 const { asyncHandler } = require('../middlewares/error-handler');
 
 // TODO:
-// - Implement transcrpipts schema
-// - Implement the logic to fetch transcripts from Firefiles (currently hardcoded data in MongoDB)
-const getTranscripts = asyncHandler(async (req, res) => {
-  const transcripts = await req.db.collection('transcripts').find({}).toArray();
+// - Implement meeting summary schema
+// - Implement the logic to fetch meeting summary & transcripts from Firefiles (currently hardcoded data in MongoDB)
+const getMeetingSummaries = asyncHandler(async (req, res) => {
+  const transcripts = await req.db
+    .collection('meetingsummaries')
+    .find({})
+    .toArray();
   res.status(200).send({ success: true, data: transcripts });
 });
 
 module.exports = {
-  getTranscripts
+  getMeetingSummaries
 };
--- routes/CRM.js
+++ routes/CRM.js
@@ -5,14 +5,14 @@ const { GeneralGETRequestRateLimiter } = require('../middlewares/rate_limiter');
 const { protect, permit } = require('../middlewares/auth');
 const { filter_archiv_user } = require('../middlewares/limit_archiv_user');
 
-const { getTranscripts } = require('../controllers/CRM');
+const { getMeetingSummaries } = require('../controllers/CRM');
 
 const router = Router();
 
 router.use(protect, permit(Role.Admin, Role.Agent, Role.Editor));
 
 router
-  .route('/transcripts')
-  .get(filter_archiv_user, GeneralGETRequestRateLimiter, getTranscripts);
+  .route('/meeting-summaries')
+  .get(filter_archiv_user, GeneralGETRequestRateLimiter, getMeetingSummaries);
 
 module.exports = router;

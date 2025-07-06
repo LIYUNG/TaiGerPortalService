0a7eccd14aaf4480ae8f2990d5fd1744970ef939
Alexander Jensen <ajexsen@users.noreply.github.com>
2025-07-06 18:46:39 +0200

    feat: enhance meeting summaries and leads retrieval with improved query conditions

--- controllers/crm.js
+++ controllers/crm.js
@@ -1,6 +1,7 @@
 const { asyncHandler } = require('../middlewares/error-handler');
 const { meetingTranscripts, leads } = require('../drizzle/schema/schema');
 const { postgresDb } = require('../database');
+const { sql } = require('drizzle-orm');
 
 // TODO:
 // - Implement meeting summary schema
@@ -8,12 +9,23 @@ const { postgresDb } = require('../database');
 const getMeetingSummaries = asyncHandler(async (req, res) => {
   const meetingSummaries = await await postgresDb
     .select()
-    .from(meetingTranscripts);
+    .from(meetingTranscripts)
+    .where(sql`(meeting_info->>'fred_joined')::boolean = true AND
+         (meeting_info->>'silent_meeting')::boolean = false AND
+         (meeting_info->>'summary_status') != 'skipped'`);
   res.status(200).send({ success: true, data: meetingSummaries });
 });
 
 const getCRMLeads = asyncHandler(async (req, res) => {
-  const leadsRecords = await postgresDb.select().from(leads);
+  const leadsRecords = await postgresDb
+    .select({
+      fullName: leads.fullName,
+      gender: leads.gender,
+      email: leads.email,
+      lineId: leads.lineId,
+      source: leads.source
+    })
+    .from(leads);
   res.status(200).send({ success: true, data: leadsRecords });
 });
 

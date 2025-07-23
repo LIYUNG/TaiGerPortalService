df9280f76e248e93c6a6cd51b7e0f54e2eb19890
Leo <liyung.chen@tum.de>
2025-07-23 21:33:24 +0200

    fix http log

--- services/httpLogger.js
+++ services/httpLogger.js
@@ -2,10 +2,11 @@ const morgan = require('morgan');
 const logger = require('./logger');
 
 const httpLogger = morgan((tokens, req, res) => {
+  const status = tokens.status(req, res);
   const logData = {
     method: tokens.method(req, res),
     url: tokens.url(req, res),
-    status: tokens.status(req, res),
+    status,
     contentLength: tokens.res(req, res, 'content-length'),
     headers: req.headers,
     ipAddress: req.ip,
@@ -13,7 +14,11 @@ const httpLogger = morgan((tokens, req, res) => {
     responseTime: tokens['response-time'](req, res)
     // Additional context as needed
   };
-  logger.log('info', `${JSON.stringify(logData)}`);
+  if (status >= 400) {
+    logger.error(`${JSON.stringify(logData)}`);
+  } else {
+    logger.info(`${JSON.stringify(logData)}`);
+  }
 });
 
 module.exports = httpLogger;

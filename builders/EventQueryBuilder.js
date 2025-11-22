58614731d69cda31a74db11cf1c61f3f51afc147
LIYUNG <40571034+LIYUNG@users.noreply.github.com>
2025-11-22 18:54:35 +0100

    Calendar improve (#107)
    * refactor event api and querybuilder
    
    * decoupled getEvents from bookedEvents
    
    * remove fetch student from getEvents
    
    * update getEvents api, remove getAllEvents api
    
    ---------
    
    Co-authored-by: Leo <liyung.chen@tum.de>

---
+++ builders/EventQueryBuilder.js
@@ -0,0 +1,81 @@
+const { BaseQueryBuilder } = require('./BaseQueryBuilder');
+
+class EventQueryBuilder extends BaseQueryBuilder {
+  constructor() {
+    super();
+    this.query = {};
+  }
+
+  withStartTimeStart(startTime) {
+    if (startTime) {
+      this.query.start = {
+        ...this.query.start,
+        $gte: new Date(startTime)
+      };
+    }
+    return this;
+  }
+
+  withStartTimeEnd(endTime) {
+    if (endTime) {
+      this.query.start = {
+        ...this.query.start,
+        $lte: new Date(endTime)
+      };
+    }
+    return this;
+  }
+
+  withEndTimeStart(startTime) {
+    if (startTime) {
+      this.query.end = {
+        ...this.query.end,
+        $gte: new Date(startTime)
+      };
+    }
+    return this;
+  }
+
+  withEndTimeEnd(endTime) {
+    if (endTime) {
+      this.query.end = {
+        ...this.query.end,
+        $lte: new Date(endTime)
+      };
+    }
+    return this;
+  }
+
+  withReceiverId(receiverId) {
+    if (receiverId) {
+      this.query.receiver_id = receiverId;
+    }
+    return this;
+  }
+
+  withRequesterId(requesterId) {
+    if (requesterId) {
+      this.query.requester_id = requesterId;
+    }
+    return this;
+  }
+
+  withConfirmedReceiver(trueOrFalse = true) {
+    this.query.isConfirmedReceiver = trueOrFalse;
+    return this;
+  }
+
+  withConfirmedRequester(trueOrFalse = true) {
+    this.query.isConfirmedRequester = trueOrFalse;
+    return this;
+  }
+
+  build() {
+    return {
+      filter: this.query,
+      options: this.options
+    };
+  }
+}
+
+module.exports = EventQueryBuilder;
